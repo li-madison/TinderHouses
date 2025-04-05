fdab3ff3839394a1c54107b7d48f5d48f8c187cf
sofiathomas715 <sofiathomas2901@gmail.com>
2025-04-05 18:57:14 -0500

    home page first

--- tinder-house/src/Pages/home.jsx
+++ tinder-house/src/Pages/home.jsx
@@ -3,7 +3,96 @@ import React from 'react'
 function home() {
   return (
     <div>
-      home
+      <div className="w-[1440px] h-[1841px] relative bg-white overflow-hidden">
+  <div className="left-[547px] top-[1030px] absolute text-center justify-start text-stone-900 text-4xl font-extrabold font-['Montserrat'] leading-[56px]">Popular Offers</div>
+  <div className="w-96 h-[534px] left-[87px] top-[1150px] absolute bg-stone-300 rounded-2xl shadow-[0px_4px_30px_0px_rgba(43,27,18,0.12)] overflow-hidden">
+    <div className="w-96 h-96 left-0 top-0 absolute bg-zinc-300" />
+    <img className="w-96 h-96 left-[-17px] top-[-1px] absolute" src="https://placehold.co/402x402" />
+    <img className="w-6 h-6 left-[20px] top-[374px] absolute" src="https://placehold.co/24x24" />
+    <div className="left-[52px] top-[374px] absolute justify-start text-stone-900 text-xl font-bold font-['Montserrat']">San Francisco, California</div>
+    <div className="left-[226px] top-[483px] absolute justify-start text-stone-700 text-xl font-bold font-['Montserrat']">$2,500,000</div>
+    <div className="w-6 h-6 left-[20px] top-[423px] absolute bg-white rounded" />
+    <img className="w-6 h-6 left-[20px] top-[423px] absolute" src="https://placehold.co/24x24" />
+    <div className="left-[52px] top-[424px] absolute justify-start text-stone-700 text-lg font-semibold font-['Montserrat']">4 Rooms</div>
+    <div className="w-6 h-6 left-[173px] top-[424px] absolute bg-white rounded" />
+    <div className="left-[205px] top-[425px] absolute justify-start text-stone-700 text-lg font-semibold font-['Montserrat']">3,500 sq ft</div>
+    <img className="w-6 h-6 left-[173px] top-[424px] absolute" src="https://placehold.co/24x24" />
+    <div className="w-24 h-10 left-[20px] top-[472px] absolute bg-stone-900 rounded-lg" />
+    <div className="left-[41px] top-[483px] absolute justify-start text-white text-sm font-semibold font-['Montserrat']">Sign up</div>
+  </div>
+  <div className="w-96 h-[534px] left-[529px] top-[1150px] absolute bg-stone-300 rounded-2xl shadow-[0px_4px_30px_0px_rgba(43,27,18,0.12)] overflow-hidden">
+    <div className="w-96 h-96 left-0 top-0 absolute bg-zinc-300" />
+    <img className="w-96 h-96 left-[-6px] top-[-1px] absolute opacity-95" src="https://placehold.co/392x392" />
+    <img className="w-6 h-6 left-[20px] top-[374px] absolute" src="https://placehold.co/24x24" />
+    <div className="left-[52px] top-[374px] absolute justify-start text-stone-900 text-xl font-bold font-['Montserrat']">Beverly Hills, California</div>
+    <div className="left-[245px] top-[483px] absolute justify-start text-stone-700 text-xl font-bold font-['Montserrat']">$850,000</div>
+    <div className="w-6 h-6 left-[20px] top-[423px] absolute bg-white rounded" />
+    <img className="w-6 h-6 left-[20px] top-[423px] absolute" src="https://placehold.co/24x24" />
+    <div className="left-[52px] top-[424px] absolute justify-start text-stone-700 text-lg font-semibold font-['Montserrat']">3 Rooms</div>
+    <div className="w-6 h-6 left-[173px] top-[424px] absolute bg-white rounded" />
+    <div className="left-[205px] top-[425px] absolute justify-start text-stone-700 text-lg font-semibold font-['Montserrat']">1,500 sq ft</div>
+    <img className="w-6 h-6 left-[173px] top-[424px] absolute" src="https://placehold.co/24x24" />
+    <div className="w-24 h-10 left-[20px] top-[472px] absolute bg-stone-900 rounded-lg" />
+    <div className="left-[41px] top-[483px] absolute justify-start text-white text-sm font-semibold font-['Montserrat']">Sign up</div>
+  </div>
+  <div className="w-96 h-[534px] left-[971px] top-[1150px] absolute bg-stone-300 rounded-2xl shadow-[0px_4px_30px_0px_rgba(43,27,18,0.12)] overflow-hidden">
+    <div className="w-96 h-96 left-0 top-0 absolute bg-zinc-300" />
+    <img className="w-96 h-96 left-[-14px] top-[-24px] absolute" src="https://placehold.co/424x424" />
+    <img className="w-6 h-6 left-[20px] top-[374px] absolute" src="https://placehold.co/24x24" />
+    <div className="left-[52px] top-[374px] absolute justify-start text-stone-900 text-xl font-bold font-['Montserrat']">Palo Alto, California</div>
+    <div className="left-[226px] top-[483px] absolute justify-start text-stone-700 text-xl font-bold font-['Montserrat']">$3,700,000</div>
+    <div className="w-6 h-6 left-[20px] top-[423px] absolute bg-white rounded" />
+    <img className="w-6 h-6 left-[20px] top-[423px] absolute" src="https://placehold.co/24x24" />
+    <div className="left-[52px] top-[424px] absolute justify-start text-stone-700 text-lg font-semibold font-['Montserrat']">6 Rooms</div>
+    <div className="w-6 h-6 left-[173px] top-[424px] absolute bg-white rounded" />
+    <div className="left-[205px] top-[425px] absolute justify-start text-stone-700 text-lg font-semibold font-['Montserrat']">4,000 sq ft</div>
+    <img className="w-6 h-6 left-[173px] top-[424px] absolute" src="https://placehold.co/24x24" />
+    <div className="w-24 h-10 left-[20px] top-[472px] absolute bg-stone-900 rounded-lg" />
+    <div className="left-[41px] top-[483px] absolute justify-start text-white text-sm font-semibold font-['Montserrat']">Sign up</div>
+  </div>
+  <div className="w-[1440px] h-[820px] left-0 top-[3087px] absolute bg-red-50" />
+  <div className="w-[1440px] h-[539px] left-0 top-[4492px] absolute bg-stone-300" />
+  <div className="w-[1440px] h-[911px] left-0 top-0 absolute bg-red-50 overflow-hidden">
+    <div className="w-24 h-7 left-[97px] top-[49px] absolute overflow-hidden">
+      <div className="left-[4.52px] top-[4.52px] absolute justify-start text-stone-900 text-2xl font-extrabold font-['Montserrat']">miCasa</div>
+    </div>
+    <div className="left-[455px] top-[53px] absolute justify-start text-stone-900 text-lg font-bold font-['Montserrat']">Preferences</div>
+    <div className="left-[626px] top-[53px] absolute justify-start text-stone-900 text-lg font-bold font-['Montserrat']">Finder</div>
+    <div className="left-[744px] top-[53px] absolute justify-start text-stone-900 text-lg font-bold font-['Montserrat']">Advisor</div>
+    <div className="left-[878px] top-[53px] absolute justify-start text-stone-900 text-lg font-bold font-['Montserrat']">Finances</div>
+    <div className="w-6 h-6 left-[1090px] top-[52px] absolute overflow-hidden">
+      <div className="w-5 h-5 left-[1px] top-[1px] absolute bg-stone-900" />
+    </div>
+    <div className="w-6 h-6 left-[1146px] top-[52px] absolute overflow-hidden">
+      <div className="w-3.5 h-5 left-[5px] top-[1px] absolute bg-stone-900" />
+    </div>
+    <div className="w-32 h-12 left-[1214px] top-[40px] absolute bg-stone-900 rounded-lg" />
+    <div className="left-[1243px] top-[54px] absolute justify-start text-white text-base font-semibold font-['Montserrat']">Sign up</div>
+    <div className="w-44 h-14 left-[104px] top-[608px] absolute bg-stone-900 rounded-lg" />
+    <div className="left-[156px] top-[626px] absolute justify-start text-white text-lg font-semibold font-['Montserrat']">Sign up</div>
+    <div className="w-[514px] left-[104px] top-[245px] absolute justify-start"><span class="text-stone-900 text-7xl font-extrabold font-['Montserrat']">Find Your</span><span class="text-stone-900 text-7xl font-extrabold font-['Montserrat']"> Dream Home</span></div>
+    <div className="w-96 left-[103px] top-[449px] absolute justify-start text-stone-700 text-lg font-bold font-['Montserrat'] leading-7">blahblahblah<br/>-<br/>-<br/>-</div>
+  </div>
+  <div className="w-[1070px] h-40 left-[184px] top-[820px] absolute bg-stone-300 rounded-2xl overflow-hidden">
+    <div className="w-64 h-14 left-[31px] top-[51px] absolute bg-red-50 rounded-lg" />
+    <div className="left-[46px] top-[69px] absolute justify-start text-stone-600 text-lg font-bold font-['Montserrat']">Location</div>
+    <div className="w-5 h-5 left-[246px] top-[69px] absolute">
+      <div className="w-3.5 h-4 left-[4px] top-[2px] absolute bg-stone-600" />
+    </div>
+    <div className="w-64 h-14 left-[307px] top-[51px] absolute bg-red-50 rounded-lg" />
+    <div className="left-[322px] top-[69px] absolute justify-start text-stone-600 text-lg font-bold font-['Montserrat']">Type</div>
+    <div className="w-5 h-5 left-[522px] top-[69px] absolute">
+      <div className="w-4 h-4 left-[3px] top-[3.21px] absolute bg-stone-600" />
+    </div>
+    <div className="w-64 h-14 left-[583px] top-[51px] absolute bg-red-50 rounded-lg" />
+    <div className="left-[598px] top-[69px] absolute justify-start text-stone-600 text-lg font-bold font-['Montserrat']">Price Range</div>
+    <div className="w-5 h-5 left-[798px] top-[69px] absolute">
+      <div className="w-5 h-4 left-0 top-[2px] absolute bg-stone-600" />
+    </div>
+    <div className="w-40 h-14 left-[881px] top-[51px] absolute bg-stone-900 rounded-lg" />
+    <div className="left-[924px] top-[69px] absolute justify-start text-white text-lg font-semibold font-['Montserrat']">Sign up</div>
+  </div>
+</div>
     </div>
   )
 }
