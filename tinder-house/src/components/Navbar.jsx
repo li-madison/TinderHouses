01ac6283e28986b322dd4d60518be1aedb27a053
li-madison <forsterliana@gmail.com>
2025-04-05 21:33:43 -0500

    navbar

--- tinder-house/src/components/Navbar.jsx
+++ tinder-house/src/components/Navbar.jsx
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
+    <div className ="flex flex-row items-center px-20 py-4 justify-between bg-[#f9dac4 lg:">
     
-    <div className = "flex flex-row justify-between items-center px-20 py-4 bg-[#f9dac4]">
-      <Link to ="/" className=" justify-start text-stone-900 text-2xl font-extrabold font-['Montserrat']">miCasa</Link>
+      <Link to ="/" className="text-stone-900 text-2xl font-extrabold font-['Montserrat']">miCasa</Link>
  
-    
-    <Link to ="/houses" className=" text-stone-900 text-lg font-bold font-['Montserrat']">Finder</Link>
+  
+    <Link to ="/houses" className=" text-stone-900 text-lg font-bold font-['Montserrat']">Matches</Link>
     <Link to ="/finance" className=" text-stone-900 text-lg font-bold font-['Montserrat']">Finances</Link>
+    
+
     <Link to ="/Profile"><VscAccount className = " text-stone-900 transition duration-300" size = "2rem"/></Link>
     </div>
   
